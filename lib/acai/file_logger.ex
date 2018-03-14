a1242d4a149d0b270c9cd93d0befe7e503bcc06f
Alvin Crespo <alvin.crespo@gmail.com>
2018-03-13 20:13:20 -0400

    Checks to see if the directory exists

--- lib/acai/file_logger.ex
+++ lib/acai/file_logger.ex
@@ -1,5 +1,33 @@
 defmodule Acai.FileLogger do
   def log(name, msg) do
+    path = "./logs/"
+
+    dir_exists?(path)
+      |> verify_dir()
+      |> write_to_file(name, msg)
+  end
+
+  defp dir_exists?(path) do
+    if File.dir?(path) do
+      {true, path}
+    else
+      {false, path}
+    end
+  end
+
+  defp verify_dir({true, path}) do
+    path
+  end
+
+  defp verify_dir({false, path}) do
+    with :ok <- File.mkdir(path) do
+      path
+    end
+  end
+
+  defp write_to_file(path, name, msg) do
+    filename = "#{name}.log"
+
     io_list = [
       pretty_shorttime(:erlang.system_time()),
       to_string(name),
@@ -8,7 +36,7 @@ defmodule Acai.FileLogger do
       "\n"
     ]
 
-    File.write!("./logs/#{name}.log", io_list, [:append])
+    File.write!(path <> filename, io_list, [:append])
   end
 
   defp pretty_shorttime(time) do
